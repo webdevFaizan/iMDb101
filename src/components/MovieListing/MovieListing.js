96f286f9ee7bdd655a63e963c8b22892efc5d3dd
webdevFaizan <webdev.Faizan@gmail.com>
2022-10-07 14:46:59 +0530

    The carousel is added, although not how I wanted it but working, react-slick is not working properly as it was shown in the video.

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -3,18 +3,29 @@ import { useSelector } from 'react-redux'
 import { getAllMovies,getAllSeries } from '../../features/movies/movieSlice';
 import MovieCard from '../MovieCard/MovieCard'
 import './MovieListing.scss'
+// import "react-responsive-carousel/lib/styles/carousel.min.css";
+// import {Carousel} from 'react-responsive-carousel';
+// import Carousel from 'react-elastic-carousel'
+// import ReactSimplyCarousel from 'react-simply-carousel';
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css"; 
+// import "slick-carousel/slick/slick-theme.css";
+// import settings  from '../../common/settings.js';
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 export default function MovieListing() {
   const movies = useSelector(getAllMovies);
   const series = useSelector(getAllSeries);
 
   let renderMovies = "";
-  let renderShows = "";  
+  let renderShows = "";   
   
   renderMovies = movies.Response ==='True' ? (
-    movies.Search.map((movie, index) => (
-      <MovieCard key={index} data={movie} />
-    ))
+    movies.Search.map((movie, index) => {
+      return (<MovieCard key={index} data={movie} />)
+    })
   ):(
     <div className="movies-error">
       <h3>{movies.Error}</h3>
@@ -23,7 +34,7 @@ export default function MovieListing() {
 
   renderShows = series.Response ==='True' ? (
     series.Search.map((movie, index) => (
-      <MovieCard key={index} data={movie} />
+      <MovieCard data-index={index} key={index} data={movie} />
     ))
   ):(
     <div className="movies-error">
@@ -31,17 +42,29 @@ export default function MovieListing() {
     </div>
   );
 
-  
-  // console.log(renderMovies);
   return (
     <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
-        <div className="movie-container">{renderMovies}</div>
+        <div className="movie-container">
+          <div className="movie-inner-container">
+            <Carousel itemsToShow={1}>
+            {/* <ReactSimplyCarousel itemsToShow={1} itemsToScroll={1} offsetWidth={499}> */}
+            {/* <Slider {...settings}> */}
+              {renderMovies}
+            {/* </Slider> */}
+            {/* </ReactSimplyCarousel> */}
+            </Carousel>
+          </div>  
+        </div>
       </div>
       <div className="show-list">
         <h2>Shows</h2>
-        <div className="movie-container">{renderShows}</div>
+        <div className="movie-container">
+          {/* <Slider {...settings}> */}
+            {renderShows}
+          {/* </Slider> */}
+        </div>
       </div>
     </div>
   )
